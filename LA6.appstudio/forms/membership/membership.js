1aaff0742f8ab873488847ad1fdb13ca80280c6a
Grant Whittelsey <gmw92396@creighton.edu>
2021-09-29 15:30:47 -0500

    FinalCommit

--- LA6.appstudio/forms/membership/membership.js
+++ LA6.appstudio/forms/membership/membership.js
@@ -1,4 +1,16 @@
 /*
 let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+
+showMsg.onclick=function(){
+let username = InputName.value
+for (i = 0; i < members.length; i++)
+  if (members.includes(username))
+    alertOutput.value = username + " is already a member."
+  else{
+    members.push()
+    alertOutput.value = username + " has been added as a mamber."
+  
+  }
+}
+*/
