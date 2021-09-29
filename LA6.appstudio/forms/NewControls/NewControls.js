468e34ea85e18639124ca1558ab4a228a782833e
Grant Whittelsey <gmw92396@creighton.edu>
2021-09-29 15:27:26 -0500

    Progress 2

---
+++ LA6.appstudio/forms/NewControls/NewControls.js
@@ -0,0 +1,25 @@
+
+Header.onclick=function(){
+  console.log("Thank you for clicking the header.")
+}
+
+
+
+
+TextA.onclick=function(){
+   console.log("Thank you for clicking the textbox.")
+}
+
+
+
+Consent.onclick=function(){
+    console.log("Thank you for your feedback.")
+  
+}
+
+
+
+
+Sig1.onclick=function(){
+    console.log("Thank you for clicking the Signiture.")
+}
---
+++ LA6.appstudio/forms/calculator/calculator.js
@@ -0,0 +1,26 @@
+
+/*
+btnAdd.onclick=function(){
+  let num1 = Number(INPfirst.value)
+  let num2 = Number(INPsecond.value)
+  let answer = num1 + num2
+  
+  lblOutput.value = answer
+
+}
+
+btnMult.onclick=function(){
+  let num1 = Number(INPfirst.value)
+  let num2 = Number(INPsecond.value)
+    let answer = num1 * num2
+  
+  lblOutput.value = answer
+}
+
+Clear.onclick=function(){
+  INPfirst.value= ""
+  INPsecond.value= ""
+  lblOutput.value= ""
+  
+}
+*/
